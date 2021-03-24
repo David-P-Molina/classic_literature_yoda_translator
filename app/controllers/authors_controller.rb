ee898747581c9dcd7e285de8edd389bab335a670
David Moina <davemolina777@gmail.com>
2021-03-24 17:49:17 -0500

    Added basic controller functionality

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -1,13 +1,41 @@
 class AuthorsController < ApplicationController
+  before_action :set_author, only: [:show, :edit]
   def index
+    @authors = Author.all
+  end
+  
+  def show
   end
 
   def new
+    @author = Author.new
+  end
+
+  def create
+    @author = Author.new(author_params)
+    if @author.save
+      redirect_to authors_path
+    else 
+      render :new
+    end
   end
 
   def edit
   end
 
-  def show
+  def update
+    @author =Author.find(params[:id])
+    if @author.update(author_params)
+      redirect_to authors_path
+    else 
+      render :edit
+    end
+  end
+  private
+  def set_author
+    @author = Author.find(params[:id])
+  end
+  def author_params
+    params.require(:author).permit(:name, classics_id: [])
   end
 end
