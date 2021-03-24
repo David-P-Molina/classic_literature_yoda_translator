960990c057488335f02ee6082c7486cca60abcb7
David Moina <davemolina777@gmail.com>
2021-03-24 17:34:14 -0500

    added controllers and views

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,15 @@
 Rails.application.routes.draw do
+  get 'classic/index'
+  get 'classic/new'
+  get 'classic/edit'
+  get 'classic/show'
+  get 'categories/index'
+  get 'categories/new'
+  get 'categories/edit'
+  get 'categories/show'
+  get 'authors/index'
+  get 'authors/new'
+  get 'authors/edit'
+  get 'authors/show'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
