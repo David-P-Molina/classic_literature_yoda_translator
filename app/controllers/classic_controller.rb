7be6c0db6f28695eeef2eab14c3007d7a30b7c9a
David Moina <davemolina777@gmail.com>
2021-03-24 18:02:58 -0500

    Added basic actions to controller

--- app/controllers/classic_controller.rb
+++ app/controllers/classic_controller.rb
@@ -1,13 +1,40 @@
 class ClassicController < ApplicationController
+  before_action :set_classic, only: [:edit, :show]
+  
   def index
+    @classics = Classic.new
+  end
+  def show
   end
 
   def new
+    @classic = Classic.new
+  end
+  def create
+    @classic = Classic.new(classic_params)
+    if @classic.save
+      redirect_to classics_path
+    else
+      render :new
+    end
   end
 
   def edit
   end
 
-  def show
+  def update
+    @classic = Classic.find(params[:id])
+    if @classic.update(classic_params)
+      redirect_to classics_path
+    else
+      render :edit
+    end
+  end
+  private
+  def set_classic
+    @classic = Classic.find(params[:id])
+  end
+  def classic_params
+    params.require(:classic).permit!
   end
 end
