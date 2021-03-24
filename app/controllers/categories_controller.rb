8a082d1dfbcc3da3984a38a194933aa0a1fa4798
David Moina <davemolina777@gmail.com>
2021-03-24 17:56:05 -0500

    Added  basic controller actions

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,13 +1,37 @@
 class CategoriesController < ApplicationController
+  before_action :set_category, only: [:edit, :show]
   def index
+    @category = Category.all
   end
-
+  def show
+  end
   def new
+      @category = Category.new
+  end
+  def create
+    @category = Category.new(category_params)
+    if @category.save
+      redirect_to categories_path
+    else
+      render :new
+    end
   end
 
   def edit
   end
-
-  def show
+  def update
+    @category = Category.find(params[:id])
+    if @category.update(category_params)
+      redirect_to category_path
+    else
+      render :edit
+    end
+  end
+  private
+  def set_category
+    @category = Category.find(params[:id])
+  end
+  def category_params
+    params.require(:category).permit(:name, classic_ids: [])
   end
 end
