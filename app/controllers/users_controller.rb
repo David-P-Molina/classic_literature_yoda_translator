1d38ab137f18714acd05a838292f4fdd48cc32a7
David Moina <davemolina777@gmail.com>
2021-04-02 15:13:56 -0500

    Added user creation functionality

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,9 +2,21 @@ class UsersController < ApplicationController
   def new
   end
 
+  def create
+    User.create(user_params)
+  end
+
   def edit
+  end
+  
+  def update
   end
 
   def show
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:username, :password, :password_confirmation, :email)
   end
 end
