7b4529ae89474c598b546299ba04dc184a3e9129
David Moina <davemolina777@gmail.com>
2021-04-02 11:15:23 -0500

    Added pseudo code for api

--- app/models/api.rb
+++ app/models/api.rb
@@ -1,2 +1,5 @@
 class Api < ApplicationRecord
+    #method that sends information to api
+    #method that retrieves information
+    #method that assigns to either translation class or yoda column in classics table
 end
