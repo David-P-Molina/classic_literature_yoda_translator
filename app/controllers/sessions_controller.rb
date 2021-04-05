45dd6b917612699760c6ae4bfcd411f10b05e8b9
David Moina <davemolina777@gmail.com>
2021-04-05 11:58:22 -0500

    Added create and destroy method

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,7 +3,9 @@ class SessionsController < ApplicationController
 
     end
     def create
-
+        @user = User.find_by(username: params[:username])
+        return head(:forbidden) unless @user.authenticate(params[:password])
+        session[:user_id] = @user.id
     end
     def destroy
         session.delete :username
