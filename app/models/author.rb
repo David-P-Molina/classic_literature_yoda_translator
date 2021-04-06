7155c1d17b5d34dec2a53fe80b8ee8fd22205a8f
David Moina <davemolina777@gmail.com>
2021-04-06 18:47:24 -0500

    Changed typos in seed file and added lifetime method to author

--- app/models/author.rb
+++ app/models/author.rb
@@ -3,4 +3,8 @@ class Author < ApplicationRecord
     has_many :categories, through: :classics
     validates :name, uniqueness: true
     validates :biography, presence: true
+
+    def life_time
+        self.birth.to_s + " - " + self.death.to_s
+    end
 end
