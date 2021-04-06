d16d3bf33787f04a331a1b91e985aa7a1c6144a8
David Moina <davemolina777@gmail.com>
2021-04-06 14:27:58 -0500

    Changed route and added basic user show

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,7 +7,7 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
     if @user.save
       session[:user_id] = @user.id
-      redirect_to '/classics/index'
+      redirect_to controller: 'users', action: 'show'
     else
       #add flash error
       redirect_to controller: 'users', action: 'new'
@@ -26,6 +26,7 @@ class UsersController < ApplicationController
   # end
 
   def show
+    @user = User.find(params[:id])
   end
 
   private
