fe8ee845351abfa76be342b73b22c589c228ca4c
David Moina <davemolina777@gmail.com>
2021-04-07 11:23:01 -0500

    Added basic outline for api

--- app/models/api.rb
+++ app/models/api.rb
@@ -2,4 +2,11 @@ class Api < ApplicationRecord
     #method that sends information to api
     #method that retrieves information
     #method that assigns to either translation class or yoda column in classics table
+    def get_translation
+        url = "https://api.funtranslations.com/translate/yoda.json"
+        uri = URI.parse(url)
+        response = Net::HTTP.get_response(uri)
+        response.body
+    end
+
 end
