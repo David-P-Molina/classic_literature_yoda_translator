b904acb62fa3018e9d3581479653645a7b489929
David Moina <davemolina777@gmail.com>
2021-04-07 11:44:47 -0500

    Basic flow for api laid out

--- app/models/api.rb
+++ app/models/api.rb
@@ -1,12 +1,18 @@
 class Api < ApplicationRecord
-    #method that sends information to api
-    #method that retrieves information
-    #method that assigns to either translation class or yoda column in classics table
-    def get_translation
-        url = "https://api.funtranslations.com/translate/yoda.json"
-        uri = URI.parse(url)
-        response = Net::HTTP.get_response(uri)
-        response.body
-    end
+    #method that sends information to api and sets endpoint as url to replace
 
+    #method that retrieves information
+    # def self.get_translation
+        # url = "https://api.funtranslations.com/translate/yoda.json" #endpoint
+        # uri = URI.parse(url)
+        # response = Net::HTTP.get_response(uri)
+        # response.body
+    # end
+    
+    #method that assigns to either translation class or yoda column in classics table
+    # def self.yoda_translation#if translation class exists
+        # classic_info = JSON.parse(self.get_translation)
+        #classic = Classic.find(params[:id])
+        # classic.translation = classic_info[contents][translated] 
+    # end
 end
