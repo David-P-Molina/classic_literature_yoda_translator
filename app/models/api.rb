6962542849816a89b3e44e4712fd17b16a0117d8
David Moina <davemolina777@gmail.com>
2021-04-07 13:13:00 -0500

    Fixed api information. needs work

--- app/models/api.rb
+++ app/models/api.rb
@@ -1,21 +1,26 @@
-class Api < ApplicationRecord
-    #method that sends information to api and sets endpoint as url to replace
-    # def self.send_classic
-        #send to API
-        # @url = api_response 
-        ##example @url = "https://api.funtranslations.com/translate/yoda.json" #endpoint
-    # end
-    #method that retrieves information
-    # def self.get_translation
-        # uri = URI.parse(@url)
-        # response = Net::HTTP.get_response(uri)
-        # response.body
-    # end
-    
-    #method that assigns to either translation class or yoda column in classics table
-    # def self.yoda_translation#if translation class exists
-        # classic_info = JSON.parse(self.get_translation)
-        #classic = Classic.find(params[:id])
-        # classic.translation = classic_info[contents][translated] 
-    # end
+require_relative '../../config/environment.rb'
+class Api 
+   
+   def self.set_classic(content)
+       "https://api.funtranslations.com/translate/yoda.json?text=" + URI::Parser.new.escape(content)
+   end
+    def self.start_translation
+        Classic.all.each do |classic|
+            url = set_classic(classic.content)
+            classic.translation = yoda_translation(url)
+        end
+    end
+
+    def self.get_translation(url)
+        uri = URI.parse(url)
+        response = Net::HTTP.get_response(uri)
+        response.body
+    end
+    def self.yoda_translation(url) #set
+        classic_info = JSON.parse(self.get_translation(url))
+        classic_info['contents']['translated'] 
+    end
 end
+{
+    {"success": {"total": 1},"contents": {"translated": "If there is some mistake,  to ask","text": "To ask if there is some mistake","translation": "yoda"}}
+Api.start_translation
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,18 +1,18 @@
 #Category Seed Info: Admin Edit Only
-Category.create(name: "Song", definition: "A short poem or other set of words set to music or meant to be sung.")
-Category.create(name: "Prose", definition: "Written or spoken language in its ordinary form, without metrical structure.")
-Category.create(name: "Epic", definition: "A long poem, typically one derived from ancient oral tradition, narrating the deeds and adventures of heroic or legendary figures or the history of a nation.")
-Category.create(name: "Poem", definition: "A piece of writing that partakes of the nature of both speech and song that is nearly always rhythmical, usually metaphorical, and often exhibits such formal elements as meter, rhyme, and stanzaic structure.")
-Category.create(name: "Lyric", definition: "Expressing the writer's emotions, usually briefly and in stanzas or recognized forms.")
-Category.create(name: "Autobiography", definition: "An account of a person's life written by that person.")
-Category.create(name: "Biography", definition: "An account of someone's life written by someone else.")
-Category.create(name: "Short Story", definition: "A story with a fully developed theme but significantly shorter and less elaborate than a novel.")
-Category.create(name: "Novel", definition: "A ictitious prose narrative of book length, typically representing character and action with some degree of realism.")
-Category.create(name: "Fable", definition: "A short story, typically with animals as characters, conveying a moral.")
-Category.create(name: "Religious Text", definition: "Writing that is venerated for the worship of a deity")
+Category.find_or_create_by(name: "Song", definition: "A short poem or other set of words set to music or meant to be sung.")
+Category.find_or_create_by(name: "Prose", definition: "Written or spoken language in its ordinary form, without metrical structure.")
+Category.find_or_create_by(name: "Epic", definition: "A long poem, typically one derived from ancient oral tradition, narrating the deeds and adventures of heroic or legendary figures or the history of a nation.")
+Category.find_or_create_by(name: "Poem", definition: "A piece of writing that partakes of the nature of both speech and song that is nearly always rhythmical, usually metaphorical, and often exhibits such formal elements as meter, rhyme, and stanzaic structure.")
+Category.find_or_create_by(name: "Lyric", definition: "Expressing the writer's emotions, usually briefly and in stanzas or recognized forms.")
+Category.find_or_create_by(name: "Autobiography", definition: "An account of a person's life written by that person.")
+Category.find_or_create_by(name: "Biography", definition: "An account of someone's life written by someone else.")
+Category.find_or_create_by(name: "Short Story", definition: "A story with a fully developed theme but significantly shorter and less elaborate than a novel.")
+Category.find_or_create_by(name: "Novel", definition: "A ictitious prose narrative of book length, typically representing character and action with some degree of realism.")
+Category.find_or_create_by(name: "Fable", definition: "A short story, typically with animals as characters, conveying a moral.")
+Category.find_or_create_by(name: "Religious Text", definition: "Writing that is venerated for the worship of a deity")
 
-Author.create(name: "John Donne", biography: "John Donne (/dʌn/ DUN; 22 January 1572[1] – 31 March 1631) was an English poet, scholar, soldier and secretary born into a recusant family, who later became a cleric in the Church of England.[3] Under royal patronage, he was made Dean of St Paul's Cathedral in London (1621–1631).[2] He is considered the preeminent representative of the metaphysical poets. His poetical works are noted for their metaphorical and sensual style and include sonnets, love poems, religious poems, Latin translations, epigrams, elegies, songs, and satires. He is also known for his sermons.")
-Classic.create(title: "No Man Is An Island", content: "No man is an island,
+Author.find_or_create_by(name: "John Donne", biography: "John Donne (/dʌn/ DUN; 22 January 1572[1] – 31 March 1631) was an English poet, scholar, soldier and secretary born into a recusant family, who later became a cleric in the Church of England.[3] Under royal patronage, he was made Dean of St Paul's Cathedral in London (1621–1631).[2] He is considered the preeminent representative of the metaphysical poets. His poetical works are noted for their metaphorical and sensual style and include sonnets, love poems, religious poems, Latin translations, epigrams, elegies, songs, and satires. He is also known for his sermons.")
+Classic.find_or_create_by(author_id: 1, category_id: 2, title: "No Man Is An Island", content: "No man is an island,
     Entire of itself,
     Every man is a piece of the continent,
     A part of the main.
@@ -25,8 +25,8 @@ Classic.create(title: "No Man Is An Island", content: "No man is an island,
     Because I am involved in mankind,
     And therefore never send to know for whom the bell tolls;
     It tolls for thee.")
-    Author.create(name: "Robert Frost", biography: "Robert Lee Frost (March 26, 1874 – January 29, 1963) was an American poet. His work was initially published in England before it was published in the United States. Known for his realistic depictions of rural life and his command of American colloquial speech,[2] Frost frequently wrote about settings from rural life in New England in the early 20th century, using them to examine complex social and philosophical themes.")
-    Classic.create(title:"Stopping by Woods On a Snowy Evening", content: "Whose woods these are I think I know.
+    Author.find_or_create_by(name: "Robert Frost", biography: "Robert Lee Frost (March 26, 1874 – January 29, 1963) was an American poet. His work was initially published in England before it was published in the United States. Known for his realistic depictions of rural life and his command of American colloquial speech,[2] Frost frequently wrote about settings from rural life in New England in the early 20th century, using them to examine complex social and philosophical themes.")
+    Classic.find_or_create_by(title:"Stopping by Woods On a Snowy Evening", content: "Whose woods these are I think I know.
         His house is in the village though;
         He will not see me stopping here
         To watch his woods fill up with snow.
@@ -42,8 +42,8 @@ Classic.create(title: "No Man Is An Island", content: "No man is an island,
         But I have promises to keep,
         And miles to go before I sleep,
         And miles to go before I sleep.")
-    Author.create(name: "Maya Angelou", biography: "Maya Angelou (/ˈændʒəloʊ/ (About this soundlisten) ANN-jəl-oh;[1][2] born Marguerite Annie Johnson; April 4, 1928 – May 28, 2014) was an American poet, memoirist, and civil rights activist. She published seven autobiographies, three books of essays, several books of poetry, and is credited with a list of plays, movies, and television shows spanning over 50 years. She received dozens of awards and more than 50 honorary degrees.[3] Angelou is best known for her series of seven autobiographies, which focus on her childhood and early adult experiences. The first, I Know Why the Caged Bird Sings (1969), tells of her life up to the age of 17 and brought her international recognition and acclaim.")
-    Classic.create(title:"Still I Rise", content: "You may write me down in history
+    Author.find_or_create_by(name: "Maya Angelou", biography: "Maya Angelou (/ˈændʒəloʊ/ (About this soundlisten) ANN-jəl-oh;[1][2] born Marguerite Annie Johnson; April 4, 1928 – May 28, 2014) was an American poet, memoirist, and civil rights activist. She published seven autobiographies, three books of essays, several books of poetry, and is credited with a list of plays, movies, and television shows spanning over 50 years. She received dozens of awards and more than 50 honorary degrees.[3] Angelou is best known for her series of seven autobiographies, which focus on her childhood and early adult experiences. The first, I Know Why the Caged Bird Sings (1969), tells of her life up to the age of 17 and brought her international recognition and acclaim.")
+    Classic.find_or_create_by(title:"Still I Rise", content: "You may write me down in history
         With your bitter, twisted lies,
         You may tread me in the very dirt
         But still, like dust, I’ll rise.
