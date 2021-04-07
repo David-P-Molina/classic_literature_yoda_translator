24070f78dafdb6d9dc88c81d20c7f0b68f2ce280
David Moina <davemolina777@gmail.com>
2021-04-06 21:20:31 -0500

    Added facebook_login action and updated routes

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,11 +1,20 @@
 class SessionsController < ApplicationController
     def new
     end
+    def facebook_login
+        @user = User.find_or_create_by(uid: auth['uid']) do |u|
+            u.username = auth['info']['name']
+            u.email = auth['info']['email']
+          end
+          session[:user_id] = @user.id
+          render 'welcome/home'
+    end
     def create
         @user = User.find_by(username: params[:username])
         return head(:forbidden) unless @user.authenticate(params[:password])
         if @user.valid?
             session[:user_id] = @user.id
+            render 'welcome/home'
         else
             redirect_to '/login'
         end
@@ -13,5 +22,9 @@ class SessionsController < ApplicationController
     def destroy
         session.delete :username
         redirect_to '/login'
+    end
+    private
+    def auth
+        request.env['omniauth.auth']
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -13,6 +13,8 @@ Rails.application.routes.draw do
   get "/login", to: "sessions#new"
   post "/login", to: "sessions#create"
   post "/logout", to: "sessions#destroy"
+  get "/auth/facebook/callback", to: "sessions#facebook_login" 
+
 
   #root 'welcome#home' add controller and views
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
