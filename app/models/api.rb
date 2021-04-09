5dcc7b6d3203d41abc90d0067f49f172ca4d62a9
David Moina <davemolina777@gmail.com>
2021-04-09 11:45:13 -0500

    changed method names for clarity

--- app/models/api.rb
+++ app/models/api.rb
@@ -4,25 +4,25 @@ class Api
    def self.format_url(content)
        "https://api.funtranslations.com/translate/yoda.json?text=" + URI::Parser.new.escape(content)
    end
-    def self.start_translation
+    
+   def self.translation
         Classic.all.each do |classic|
             if classic.translation.nil?
             url = format_url(classic.content)
-            classic.translation = yoda_translation(url)
+            classic.translation = retrieve_translation(url)
             end
         end
     end
 
-    def self.get_translation(url)
+    def self.process_translation(url)
         uri = URI.parse(url)
         response = Net::HTTP.get_response(uri)
         response.body
     end
-    def self.yoda_translation(url) #set
-        byebug
-        classic_info = JSON.parse(self.get_translation(url))
+    def self.retrieve_translation(url) 
+        classic_info = JSON.parse(self.process_translation(url))
         classic_info['contents']['translated'] 
     end
 end
 
-Api.start_translation
+Api.translation
