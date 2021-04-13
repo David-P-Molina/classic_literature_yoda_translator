cc2442563db69bcd22d7b7e1d9428a8bf1e25688
David Moina <davemolina777@gmail.com>
2021-04-13 13:34:47 -0500

    Added custom validation for release date

--- app/models/classic.rb
+++ app/models/classic.rb
@@ -2,6 +2,8 @@ class Classic < ApplicationRecord
     belongs_to :author
     belongs_to :category
     validates :title, presence: true
+    validates :release_date, presence: true
+    validate :classic_must_be_older_than_25_years
     
     def category_name=(name)
         self.category = Category.find_or_create_by(name: name)
@@ -16,5 +18,10 @@ class Classic < ApplicationRecord
     def author_name
         self.author.try(:name) #self.author ? self.author.name : nil
     end
-    
+    #Custom Validations
+    def classic_must_be_older_than_25_years
+        if release_date.present? && release_date > Date.today.year - 25
+            errors.add(:release_date, "Literature must be at least 25 years old!")
+        end
+    end
 end
