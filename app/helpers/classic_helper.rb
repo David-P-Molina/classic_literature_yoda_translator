15898d93f2e4636f5992a874940d04f86f5c986d
David Moina <davemolina777@gmail.com>
2021-04-14 11:27:45 -0500

    Added helper and author_field_tag to form

--- app/helpers/classic_helper.rb
+++ app/helpers/classic_helper.rb
@@ -1,2 +1,9 @@
 module ClassicHelper
+    def author_id_field(classic)
+        if classic.author.nil?
+            select_tag "classic[author_id]", options_from_collection_for_select(Author.all, :id, :name)
+        else
+            hidden_field_tag "classic[author_id]", classic.author_id
+        end
+    end
 end
