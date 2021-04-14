5c708039b1b3742a868f39307b902745ab653d31
David Moina <davemolina777@gmail.com>
2021-04-13 19:21:08 -0500

    Removed ability to create categories

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -17,16 +17,16 @@ class CategoriesController < ApplicationController
   #   end
   # end
 
-  def edit
-  end
-  def update
-    @category = Category.find(params[:id])
-    if @category.update(category_params)
-      redirect_to category_path
-    else
-      render :edit
-    end
-  end
+  # def edit
+  # end
+  # def update
+  #   @category = Category.find(params[:id])
+  #   if @category.update(category_params)
+  #     redirect_to category_path
+  #   else
+  #     render :edit
+  #   end
+  # end
   private
   def set_category
     @category = Category.find(params[:id])
