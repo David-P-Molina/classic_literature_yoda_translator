5866596a40e8db8a95c2be1f7e23e3f3815578ee
David Moina <davemolina777@gmail.com>
2021-04-14 19:22:45 -0500

    Changed the create method around to handle errors from API class

--- app/controllers/classics_controller.rb
+++ app/controllers/classics_controller.rb
@@ -12,13 +12,18 @@ class ClassicsController < ApplicationController
   end
   
   def create
-    @classic = Classic.create(classic_params)
+    @classic = Classic.new(classic_params)
     if @classic.valid? 
-      Api.translate(@classic)
+      translation_or_error = Api.translate(@classic)
+      if translation_or_error.include?("Too Many Requests:")
+        flash[:message] = translation_or_error
+        redirect_to classics_path
+      else
+        @classic.save
       flash[:message]="Successfully added Classic Literature to the database!"
       redirect_to classic_path(@classic)
     else
-      flash[:message]="There was an issue translating your submission."
+      flash[:message]="There was an issue translating your submission. Please Try Again"
       redirect_to classics_path
     end
   end
