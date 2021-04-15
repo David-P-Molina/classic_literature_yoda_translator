1f08948ce315dce818a26b085a74a34865c5a4e9
David Moina <davemolina777@gmail.com>
2021-04-15 13:19:38 -0500

    simplified form and formatted helper

--- app/helpers/classic_helper.rb
+++ app/helpers/classic_helper.rb
@@ -2,7 +2,7 @@ module ClassicHelper
     #view Helper
     def author_id_field(classic)
         if classic.author.nil?
-            select_tag "classic[author_id]", options_from_collection_for_select(Author.all, :id, :name)
+            select_tag "classic[author_id]", options_from_collection_for_select(Author.all, :id, :name), :include_blank => true 
         else
             hidden_field_tag "classic[author_id]", classic.author_id
         end
