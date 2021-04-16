d900e1dbf99173a16a8e0062fd766f8733d1a352
David Moina <davemolina777@gmail.com>
2021-04-16 14:17:53 -0500

    Removed comments from page

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,6 +1,5 @@
 class CategoriesController < ApplicationController
 before_action :set_category, only: [:edit, :show]
-  rescue_from ActiveRecord::RecordNotFound, with: :record_not_found
   def index
     @categories = Category.all
   end
@@ -14,8 +13,4 @@ before_action :set_category, only: [:edit, :show]
   def category_params
     params.require(:category).permit(:name, classic_ids: [])
   end
-  # def record_not_found
-  #   flash[:message] = "The page you were looking for wasn't found!"
-  #   redirect_to categories_path
-  # end
 end
