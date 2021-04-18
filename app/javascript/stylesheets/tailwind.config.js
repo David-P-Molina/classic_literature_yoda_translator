60800e4b6426bd0e92f31865025f7b97469e9ded
David Moina <davemolina777@gmail.com>
2021-04-17 19:18:25 -0500

    Added purge instructions to backend

--- app/javascript/stylesheets/tailwind.config.js
+++ app/javascript/stylesheets/tailwind.config.js
@@ -1,5 +1,7 @@
 module.exports = {
-  purge: [],
+  purge: ["./app/**/*.html.erb",
+"./app/helpers/**/*.rb",
+"./app/javascript/**/*.js"],
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
