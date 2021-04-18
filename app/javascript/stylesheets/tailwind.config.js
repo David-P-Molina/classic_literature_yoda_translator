bb8a7531bfc8308192cd2aa6947bdbde0b2805da
David Moina <davemolina777@gmail.com>
2021-04-17 19:19:41 -0500

    Added tailwind to frontend

--- app/javascript/stylesheets/tailwind.config.js
+++ app/javascript/stylesheets/tailwind.config.js
@@ -1,7 +1,9 @@
 module.exports = {
-  purge: ["./app/**/*.html.erb",
+  purge: [
+    "./app/**/*.html.erb",
 "./app/helpers/**/*.rb",
-"./app/javascript/**/*.js"],
+"./app/javascript/**/*.js"
+],
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
