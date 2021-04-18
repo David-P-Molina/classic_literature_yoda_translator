0bca5d5722f3c00e82e4567567238b895f5dc95d
David Moina <davemolina777@gmail.com>
2021-04-18 12:48:18 -0500

    Changed flash colors and reordered validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,8 @@
 class User < ApplicationRecord
   has_many :classics
   has_many :authors
-  has_secure_password
-  validates :password, length:{in: 8..30}, confirmation: true, on: :create
   validates :username, uniqueness: true, length: {in: 8..20}
   validates :email, presence: true, uniqueness: true, format: {with: /\A(?<username>[^@\s]+)@((?<domain_name>[-a-z0-9]+)\.(?<domain>[a-z]{2,}))\z/i}
+  has_secure_password
+  validates :password, length:{in: 8..30}, confirmation: true, on: :create
 end
