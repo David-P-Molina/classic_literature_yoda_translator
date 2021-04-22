2428668200a79f213c360966a52cd6579c820a7e
David Moina <davemolina777@gmail.com>
2021-04-22 08:56:24 -0500

    Fixed route for scope methods

--- config/routes.rb
+++ config/routes.rb
@@ -1,21 +1,22 @@
 Rails.application.routes.draw do
+
   root 'welcome#home'
   
+  get 'classics/oldest_classic', to: 'classics#oldest_classic'
+  get 'classics/newest_classic', to: 'classics#newest_classic'
+
   resources :authors, only: [:show, :index, :new, :create, :edit, :update] do
     resources :classics, only: [:show, :index, :new, :create, :edit, :update]
   end
   resources :classics
   resources :categories, only: [:show, :index]
-  
- 
+
+  #login & logout routes
   get "/signup", to: "users#new"
   post "/signup", to: "users#create"
   get "/login", to: "sessions#new"
   post "/login", to: "sessions#create"
-  get "/auth/facebook/callback", to: "sessions#facebook_login" 
   get "/logout", to: "sessions#destroy"
-  
-    get 'classics/ancient_classic', to: 'classics#oldest_classic'
-    get 'classics/modern_classic', to: 'classics#newest_classic'
-  
+  get "/auth/facebook/callback", to: "sessions#facebook_login" 
+
 end
